29bad87bd1a3c1674906023d2d88a85a2ee7f2f4
shanovskyi-b <shanovskyi.b@gmail.com>
2019-07-29 15:40:57 +0300

    fix data sending

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,11 @@ const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ port: 8080 });
 
-require('./call-center-data-generator')
-  .subscribe(data => {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(data));
+wss.on('connection', function connection(ws) {
+  require('./call-center-data-generator')
+    .subscribe(data => {
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(data));
       }
     });
-  });
-
-
-wss.on('connection', function open() {
 });
